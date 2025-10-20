5401e5c591d0066f107b5785182961bb23c3aa3a
zesciati <zakyabiyyu2003@gmail.com>
2025-10-21 01:50:59 +0700

    first initialize

---
+++ san_easy_max-word-of-sentences.js
@@ -0,0 +1,18 @@
+/**
+ * Direction:
+ * Find maximum number of words from given sentences.
+ *
+ * Expected:
+ * 23
+ */
+const sentences = [
+  'Mauris ultricies sed sapien eget malesuada. Suspendisse et aliquet odio, id ultrices erat. Praesent vehicula erat nulla. Aliquam a lorem urna. Donec.',
+  'Duis at tellus et ex bibendum pellentesque sed in nibh. Sed aliquet, diam id mollis facilisis, massa metus accumsan elit, at mattis magna.',
+  'Sed non nibh quam. Pellentesque eget ultrices diam. Aliquam diam justo, consectetur ac dui lobortis, vestibulum bibendum lorem. Sed porta pulvinar.',
+];
+
+function result(sentences) {
+  // your code here
+}
+
+console.log(result(sentences));
